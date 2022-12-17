0ba8688600ae3e647f45b4c93942e0fd5ffbea7b
thaiscmky <thaiscmky@github.com>
2022-12-17 15:17:53 -0500

    Remove unnecessary stuff

--- app.js
+++ app.js
@@ -5,16 +5,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve images, css files, js files from the public directory
-// Allows us to reference files with their relative path
-// Example: http://localhost:3001/images/cat.jpg
-
 app.use(express.static('public'));
 
-// Serve the cat image
-app.get('/image', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/images/cat.jpg'))
-);
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
